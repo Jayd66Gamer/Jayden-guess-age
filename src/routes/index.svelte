23239be7f48b7fcc44a33530402ef131ff6d52c3
Jayd66Gamer <jayd66gamer2868@gmail.com>
2022-01-09 01:55:23 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    let name=""
+    let age=""
+    let description=""
+    async function getAge() {
+        fetch('https://api.agify.io/?name='+ name)
+        .then((response) => response.json())
+        .then((data) => {
+  	name = data.name; // Path for temperature
+  	age = data.age; // Path for description
+	});
+}
+</script>
+
+<h1>Guess age</h1>
+
+<form on:submit|preventDefault={getAge}>
+    <label for="location">Name:</label>
+    <input id="location" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+<p>Age: {age}</p>
